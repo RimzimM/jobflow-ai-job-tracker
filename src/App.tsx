7d6bb614991b42e64f654383012f7a6b299df233
RimzimM <rimzimmaliwal08@gmail.com>
2026-09-04 14:59:37 -0700

    set up react app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="page-shell">
+      <h1>JobFlow</h1>
+      <p>Keep your job search organized in one place.</p>
+    </main>
+  )
+}
+
+export default App
